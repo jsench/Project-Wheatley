364df20bd08c8812dcb70155273376379b3ac221
robcarson3 <carson@hws.edu>
2024-04-30 09:27:34 -0400

    security

--- marlowecensus/settings.py
+++ marlowecensus/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from security.py import *
 import os
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p*(cf*(ju=2+i48)#i%-h+3fxa9zcnk5e^scqt1$_@3ytdom7s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -145,13 +142,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "marlowecensus" / "static",
 ]
 
-# EMAIL
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "carsonfpl@gmail.com"
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD','')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 
 # Django Admin URL.
 ADMIN_URL = 'admin/'
