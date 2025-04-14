ff49bac9325a0d666a23b6bb0e520a2953211bb2
AKSHITHGOUD9 <senchyne@5e604d9b56f1>
2025-04-14 20:29:08 +0000

    change

--- wheatleycensus/wsgi.py
+++ wheatleycensus/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for marlowecensus project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
+import sys
 import os
 
-from django.core.wsgi import get_wsgi_application
+# path to your project folder
+project_home = '/home/senchyne/wheatleycensus'
+if project_home not in sys.path:
+    sys.path.append(project_home)
 
+# If your Django settings file is in wheatleycensus/settings.py
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wheatleycensus.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
