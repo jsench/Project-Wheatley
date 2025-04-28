ae5f42188c9cf41e3d990b00fc0db80b58743bf1
AKSHITHGOUD9 <akshithgoud4499@gmail.com>
2025-04-28 13:05:25 -0500

    adjustments

--- wheatleycensus/views.py
+++ wheatleycensus/views.py
@@ -206,9 +206,7 @@ def static_copy(request, wc_number):
 
 def copy_page(request, wc_number):
     copy = get_object_or_404(models.Copy, wc_number=wc_number)
-    return render(request, 'census/copy_page.html', {
-        'copy': copy,
-    })
+    return render(request, 'census/copy_page.html', {'copy': copy})
 # ------------------------------------------------------------------------------
 # Issue list (per title)
 # ------------------------------------------------------------------------------
