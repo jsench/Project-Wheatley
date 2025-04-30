7f09cca957427c252d218a237687b9dfd682c889
AKSHITHGOUD9 <akshithgoud4499@gmail.com>
2025-04-29 22:53:17 -0500

    Changes

--- wheatleycensus/urls.py
+++ wheatleycensus/urls.py
@@ -5,8 +5,8 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    # path('',                views.homepage,      name='homepage'),
-    path('homepage',views.homepage,name='homepage'),
+    path('',                views.homepage,      name='homepage'),
+    # path('homepage',views.homepage,name='homepage'),
     path('search/',views.search,name='search'),
     path('search/<str:field>/<str:value>/',          views.search, name='search'),
     path('search/<str:field>/<str:value>/<str:order>/', views.search, name='search'),
