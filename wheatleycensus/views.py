5df65fbb35ad3be3e135de698ea54cfd3aae4f2c
AKSHITHGOUD9 <akshithgoud4499@gmail.com>
2025-05-14 23:33:48 -0500

    Testing

--- wheatleycensus/views.py
+++ wheatleycensus/views.py
@@ -521,15 +521,3 @@ def update_draft_copy(request, id):
         'icon_path': get_icon_path(selected_copy.issue.edition.title.id)
     }
     return render(request, 'census/copy_submission.html', context)
-
-def handler404(request, exception):
-    return render(request, '404.html', status=404)
-
-def handler500(request):
-    return render(request, '500.html', status=500)
-
-def handler403(request, exception):
-    return render(request, '403.html', status=403)
-
-def handler400(request, exception):
-    return render(request, '400.html', status=400)
